40393f2552e71359ee8dbe343f79f3afe115de07
NathanSmith123 <44278239+NathanSmith123@users.noreply.github.com>
2019-03-06 20:36:50 -0700

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for f in ~/work/Week6/cwl-data/data/structured/structued-2018*.gz ;
+for f in ~/work/Week6/cwl-data/data/structured/structured-2018*.gz ;
 do tar -zxvf ${f} ;
 done
 
